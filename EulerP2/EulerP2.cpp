977b770a3148a670ea12d51756b3dc305f180c39
Konstantin Kazantsev <kkazants@unca.edu>
2015-01-30 12:10:15 -0500

    Create Euler Problem 2 project

---
+++ EulerP2/EulerP2.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   EulerP2.cpp
+ * Author: kosti_000
+ *
+ * Created on January 30, 2015, 12:05 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
