7e1dbaecb4c9599bb3b5be84c05af4fb35497fa9
Konstantin Kazantsev <kkazants@unca.edu>
2015-02-17 16:07:52 -0500

    Added some code but really not sure how to solve this problem

--- EulerP90/EulerP90.cpp
+++ EulerP90/EulerP90.cpp
@@ -12,10 +12,31 @@
 
 using namespace std;
 
+// An array with the numbers
+const int numArray [] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+// Counter of all the arrangements
+int count = 0;
+
+// array to hold all arrangements
+int arr [][];
+
+// temporary array holder
+
+void permute (char holder [],int pos){
+    if (pos >= 2) {
+        // holder contains a completed permutation
+        cout << count << endl;
+        cout << holder << endl;
+    }
+    else{
+        // for each letter in alphabet
+        cout << "have nothing" << endl;
+            //holder[pos] = letter]
+            //permute(holder, pos+1)
+    }
+}
 
 int main(int argc, char** argv) {
     return 0;
 }
-
-
---
+++ EulerP90/build/Debug/Cygwin_4.x-Windows/EulerP90.o.d
@@ -0,0 +1,1 @@
+build/Debug/Cygwin_4.x-Windows/EulerP90.o: EulerP90.cpp
